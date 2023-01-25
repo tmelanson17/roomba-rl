9c5443870afa0132b144bdfb294bedbe19e792f2
noisebridge <secretary@noisebridge.net>
2023-01-25 15:13:45 -0800

    Replace s/local_dqn/stable baselines PPO

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ import copy
 import argparse
 import sys
 from roomba_env import RoombaEnv
-from neural_network import train, create_Q_network, test_model
+
+from stable_baselines3 import PPO
+from stable_baselines3.common.env_util import make_vec_env
+from huggingface_sb3 import package_to_hub
+
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Train a Lunar Lander model.")
@@ -20,36 +24,30 @@ def parse_args():
 
 if __name__ == '__main__':
     #env = gym.make('CartPole-v0')
-    env = RoombaEnv(max_episode_steps=1000)
+    def create_roomba_env(max_episode_steps=1000):
+        return RoombaEnv(render_mode="rgb_array", max_episode_steps=max_episode_steps)
+    
+    env = make_vec_env(create_roomba_env, n_envs=4)
+    env_id = "Roomba"
     n_actions = env.action_space.n
     args = parse_args()
-    sys.stdout=open('gamma_{}_episodes_{}_C_{}_replay_{}'.format(args.gamma, args.episodes, args.C, args.replay_memory_size), 'w')
-    output_file = './checkpoints/gamma_{}_episodes_{}_C_{}_replay_{}'.format(args.gamma, args.episodes, args.C, args.replay_memory_size)
-    model = train(env, n_actions, args)
-    model.save_weights(output_file)
-    model.load_weights(output_file)
-    env._max_episode_steps = 1000
-
-    test_model(model, env)
-    for i_episode in range(10):
-        observation = env.reset()
-        total_reward = 0
-        done = False
-        t=0
-        while not done:
-            env.render()
-            #print("State ", observation)
-            q_values = model.predict(np.array([observation]))
-            if np.random.rand() < 0.1:
-                action = np.random.randint(n_actions)
-            else:
-                action = np.argmax(q_values)
-            #print("Action; ", action)
-            observation, reward, done, info = env.step(action)
-            total_reward += reward
-            t += 1
-            if done:
-                #print("Episode finished after {} timesteps".format(t+1))
-                #print("Reward=", total_reward)
-                break
-    env.close()
+    ppo = PPO(policy='MlpPolicy', env=env, verbose=True)
+    ppo.learn(100000)
+    output_file = './ppo-default' #.format(args.gamma, args.episodes, args.C, args.replay_memory_size)
+    ppo.save(output_file)
+    #ppo.load(output_file)
+    model_architecture = 'PPO'
+    model = ppo
+    model_name = f"{model_architecture}-default"
+    # TODO: replace culteejen with username
+    repo_id = f"culteejen/{model_name}-{env_id}"
+    eval_env = make_vec_env(create_roomba_env, n_envs=4)
+    package_to_hub(
+           model=model, # Our trained model
+           model_name=model_name, # The name of our trained model
+           model_architecture=model_architecture, # The model architecture we used: in our case PPO
+           env_id=env_id, # Name of the environment
+           eval_env=eval_env, # Evaluation Environment
+           repo_id=repo_id, # id of the model repository from the Hugging Face Hub (repo_id = {organization}/{repo_name} for instance ThomasSimonini/ppo-LunarLander-v2
+           commit_message="Upload model to Hugging Face"
+    )
